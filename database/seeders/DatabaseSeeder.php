f3e9cea307c40e69d7e700df189f5cf70dcdcb42
Eduardo Venegas <evenegas06@hotmail.com>
2023-08-09 16:00:10 -0600

    make Factories...

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,22 +3,33 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+use App\Models\Category;
+use App\Models\Tag;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
+
+class DatabaseSeeder extends Seeder {
     /**
      * Seed the application's database.
      *
      * @return void
      */
-    public function run()
-    {
+    public function run() {
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // Storage::makeDirectory('posts');
+
+        $this->call(UserSeeder::class);
+
+        Category::factory(4)->create();
+        Tag::factory(8)->create();
+
+        $this->call(PostSeeder::class);
     }
 }
