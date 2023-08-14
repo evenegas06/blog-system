649f2a4d0f3b05b44ec0e5837ba8d2f77431cba2
Eduardo Venegas <evenegas06@hotmail.com>
2023-08-13 23:20:19 -0600

    add alpinejs funtionality on navbar

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -7,15 +7,13 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Image>
  */
-class ImageFactory extends Factory
-{
+class ImageFactory extends Factory {
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition()
-    {
+    public function definition() {
         return [
             'url' => $this->faker->image(public_path('storage\posts'), 640, 480, null, false),
         ];
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,7 +13,6 @@ class UserSeeder extends Seeder {
      * @return void
      */
     public function run() {
-
         User::create([
             'name'      => 'Eduardo Venegas',
             'email'     => 'evr@mail.com',
